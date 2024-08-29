d8e17fe944fa25159d85118b314c765dd1ea6e93
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2024-08-29 16:58:07 +0300

    fix lava fog color not writing + update README.md

--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Skyboxes, sun / moon
 Firework blending modes (particle.properties)  
 Text color overrides (except text.code.(16-31))  
 Cloud type override  
+Under (water / lava) fog colormaps  
 
 >Currently working on:  
-Fix under (lava/water) being a static color  
 Fog, sky colormaps  
 Nether, end sky/fog colors  
--- src/main/java/org/fabricmcpatcher/mixins/BackgroundRendererMixin.java
+++ src/main/java/org/fabricmcpatcher/mixins/BackgroundRendererMixin.java
@@ -10,8 +10,10 @@ import org.fabricmcpatcher.color.ColorizeWorld;
 import org.fabricmcpatcher.color.Colorizer;
 import org.joml.Vector4f;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
@@ -19,8 +21,40 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(BackgroundRenderer.class)
 public class BackgroundRendererMixin {
 
-    @Inject(method = "getFogColor",at= @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getBottomY()I"),locals = LocalCapture.CAPTURE_FAILHARD)
-    private static void getFogColorGetBottomY(Camera camera, float tickDelta, ClientWorld world, int clampedViewDistance, float skyDarkness, CallbackInfoReturnable<Vector4f> cir, CameraSubmersionType  cameraSubmersionType, Entity entity, float  red, float  green, float blue) {
+    @Unique
+    private static boolean drewFogCol = false;
+
+    @ModifyVariable(method = "getFogColor",at= @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getBottomY()I"),ordinal = 2)
+    private static float getFogColorGetBottomYRed(float value,Camera camera) {
+        if(camera.getSubmersionType()== CameraSubmersionType.LAVA) {
+            if(ColorizeWorld.computeUnderlavaColor()) {
+                drewFogCol=true;
+                return Colorizer.setColor[0];
+                //green=Colorizer.setColor[1];
+                //blue=Colorizer.setColor[2];
+            }
+        }
+        drewFogCol=false;
+        return value;
+    }
+    @ModifyVariable(method = "getFogColor",at= @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getBottomY()I"),ordinal = 3)
+    private static float getFogColorGetBottomYGreen(float value) {
+        if(drewFogCol) {
+            return Colorizer.setColor[1];
+        }
+        return value;
+    }
+    @ModifyVariable(method = "getFogColor",at= @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getBottomY()I"),ordinal = 4)
+    private static float getFogColorGetBottomYBlue(float value) {
+        if(drewFogCol) {
+            return Colorizer.setColor[2];
+        }
+        return value;
+    }
+
+    //@ModifyVariable(method = "getFogColor",at= @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getBottomY()I"))
+    /*@Inject(method = "getFogColor",at= @At(value = "INVOKE", target = "Lnet/minecraft/client/world/ClientWorld;getBottomY()I"),locals = LocalCapture.CAPTURE_FAILHARD)
+    private static void getFogColorGetBottomY(Camera camera, float tickDelta, ClientWorld world, int clampedViewDistance, float skyDarkness, CallbackInfoReturnable<Vector4f> cir, CameraSubmersionType cameraSubmersionType, Entity entity, float red, float green, float blue) {
         if(camera.getSubmersionType()== CameraSubmersionType.LAVA) {
             if(ColorizeWorld.computeUnderlavaColor()) {
                 red=Colorizer.setColor[0];
@@ -28,7 +62,8 @@ public class BackgroundRendererMixin {
                 blue=Colorizer.setColor[2];
             }
         }
-    }
+    }*/
+
     @Inject(method = "getFogColor",at= @At(value = "HEAD"))
     private static void getFogColorHead(Camera camera, float tickDelta, ClientWorld world, int clampedViewDistance, float skyDarkness, CallbackInfoReturnable<Vector4f> cir) {
         ColorizeWorld.setupForFog(camera.getFocusedEntity());
