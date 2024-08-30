3201bf6d8fa1c70d84e30edc63f6f1d68786a2eb
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2024-08-30 16:36:25 +0300

    fix potion cit code

---
+++ src/main/java/org/fabricmcpatcher/cit/PotionReplacer.java
@@ -0,0 +1,214 @@
+package org.fabricmcpatcher.cit;
+
+
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.potion.Potion;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
+import org.fabricmcpatcher.resource.PropertiesFile;
+import org.fabricmcpatcher.resource.TexturePackAPI;
+import org.fabricmcpatcher.utils.MCLogger;
+import org.fabricmcpatcher.utils.MCPatcherUtils;
+import org.fabricmcpatcher.utils.id.PotionIdUtils;
+
+import java.util.*;
+
+class PotionReplacer {
+    private static final MCLogger logger = MCLogger.getLogger(MCPatcherUtils.CUSTOM_ITEM_TEXTURES, "CIT");
+
+    private static final String ITEM_ID_POTION = "minecraft:potion";
+    private static final String ITEM_ID_GLASS_BOTTLE = "minecraft:glass_bottle";
+
+    private static final String LAYER_POTION_CONTENTS = TexturePackAPI.select("potion_contents", "potion_overlay");
+    private static final String LAYER_POTION_DRINKABLE = TexturePackAPI.select("potion", "potion_bottle_drinkable");
+    private static final String LAYER_POTION_SPLASH = TexturePackAPI.select("potion_splash", "potion_bottle_splash");
+    private static final String LAYER_EMPTY_BOTTLE = TexturePackAPI.select("potion", "potion_bottle_empty");
+
+    private static final int SPLASH_BIT = 0x4000;
+    private static final int EFFECT_BITS = 0x400f;
+    private static final int MUNDANE_BITS = 0x403f;
+    private static final int WATER_BITS = 0xffff;
+
+    private static final int[] POTION_EFFECTS = new int[]{
+        -1, // 0:  none
+        2,  // 1:  moveSpeed
+        10, // 2:  moveSlowdown
+        -1, // 3:  digSpeed
+        -1, // 4:  digSlowDown
+        9,  // 5:  damageBoost
+        5,  // 6:  heal
+        12, // 7:  harm
+        -1, // 8:  jump
+        -1, // 9:  confusion
+        1,  // 10: regeneration
+        -1, // 11: resistance
+        3,  // 12: fireResistance
+        -1, // 13: waterBreathing
+        14, // 14: invisibility
+        -1, // 15: blindness
+        6,  // 16: nightVision
+        -1, // 17: hunger
+        8,  // 18: weakness
+        4,  // 19: poison
+        -1, // 20: wither
+    };
+
+    private static final Map<String, Integer> mundanePotionMap = new HashMap<String, Integer>();
+    private static int weight = -2;
+
+    final List<ItemOverride> overrides = new ArrayList<ItemOverride>();
+
+    static {
+        try {
+            //https://minecraft.wiki/w/Potion?oldid=859543#Unused_potions, minus 0 & 16 & 32 which are water & akward & thick
+            for (int i : new int[]{0, 7, 11, 13, 15, 16, 23, 27, 29, 31, 32, 39, 43, 45, 47, 48, 55, 59, 61, 63}) {
+                String name = PotionIdUtils.getMundaneName(i).replaceFirst("^potion\\.prefix\\.", "");
+                mundanePotionMap.put(name, i);
+                logger.fine("%s potion -> damage value %d", name, i);
+            }
+
+            int i = 0;
+            for(Potion potion : Registries.POTION.stream().toList()) {
+                PotionIdUtils.PotionInfo info = PotionIdUtils.potionType2Info.get(Registries.POTION.getEntry(potion));
+                if(info==null)continue;
+                logger.fine("%s potion -> effect %d", info.name, i++); //potion.getName().replaceFirst("^potion\\.", "")
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    PotionReplacer() {
+        Identifier path = getPotionPath("water", false);
+        if (TexturePackAPI.hasResource(path)) {
+            weight++;
+            registerVanillaPotion(path, 0, WATER_BITS, false);
+            weight--;
+        }
+        path = getPotionPath("empty", false);
+        if (TexturePackAPI.hasResource(path)) {
+            registerEmptyBottle(path);
+        }
+        registerPotionsByEffect(false);
+        registerPotionsByEffect(true);
+        registerMundanePotions(false);
+        registerMundanePotions(true);
+        registerOtherPotions(false);
+        registerOtherPotions(true);
+    }
+
+    private static Identifier getPotionPath(String name, boolean splash) {
+        String path = "cit/potion/" + (splash ? "splash/" : "normal/") + name + ".png";
+        return TexturePackAPI.newMCPatcherIdentifier(path);
+    }
+
+    private static Properties newProperties(Identifier path, String itemID, String layer) {
+        Properties properties = new Properties();
+        properties.setProperty("type", "item");
+        properties.setProperty("items", itemID);
+        properties.setProperty("texture." + layer, path.toString());
+        properties.setProperty("texture." + LAYER_POTION_CONTENTS, "blank");
+        properties.setProperty("weight", String.valueOf(weight));
+        return properties;
+    }
+
+    private static Properties newProperties(Identifier path, String itemID, boolean splash) {
+        String layer = splash ? LAYER_POTION_SPLASH : LAYER_POTION_DRINKABLE;
+        return newProperties(path, itemID, layer);
+    }
+
+    private void registerPotionsByEffect(boolean splash) {
+
+        for (String name : PotionIdUtils.potionName2Info.keySet()) {
+            PotionIdUtils.PotionInfo info = PotionIdUtils.potionName2Info.get(name);
+        /*for (int effect = 0; effect < Potion.potionTypes.length; effect++) {
+            if (Potion.potionTypes[effect] == null) {
+                continue;
+            }*/
+
+            if(info.effect==null)continue;
+
+            Identifier path = getPotionPath(name, splash);
+            if (TexturePackAPI.hasResource(path)) {
+                if (info.hasItemForm) {//effect < POTION_EFFECTS.length && POTION_EFFECTS[effect] >= 0
+                    for (RegistryEntry<Potion> pot : info.type2Damage.keySet()) {
+
+                        int damage = PotionIdUtils.getPotionDamage(pot,splash);//POTION_EFFECTS[effect];
+
+                        //remove drinkable bit
+                        if((damage&8192)!=0)
+                            damage-=8192;
+                    /*if (splash) {
+                        damage |= SPLASH_BIT;
+                    }*/
+                        registerVanillaPotion(path, damage, EFFECT_BITS, splash);
+                    }
+                }
+                if (!splash) {
+                    registerCustomPotion(path, info.effect, splash);
+                }
+            }
+        }
+    }
+
+    private void registerMundanePotions(boolean splash) {
+        for (Map.Entry<String, Integer> entry : mundanePotionMap.entrySet()) {
+            int damage = entry.getValue();
+            if (splash) {
+                damage |= SPLASH_BIT;
+            }
+            registerMundanePotion(entry.getKey(), damage, splash);
+        }
+    }
+
+    private void registerMundanePotion(String name, int damage, boolean splash) {
+        Identifier path = getPotionPath(name, splash);
+        if (TexturePackAPI.hasResource(path)) {
+            registerVanillaPotion(path, damage, MUNDANE_BITS, splash);
+        }
+    }
+
+    private void registerOtherPotions(boolean splash) {
+        Identifier path = getPotionPath("other", splash);
+        if (TexturePackAPI.hasResource(path)) {
+            Properties properties = newProperties(path, ITEM_ID_POTION, splash);
+            StringBuilder sb = new StringBuilder();
+            for (int i : mundanePotionMap.values()) {
+                if (splash) {
+                    i |= SPLASH_BIT;
+                }
+                sb.append(' ').append(i);
+            }
+            properties.setProperty("damage", sb.toString().trim());
+            properties.setProperty("damageMask", String.valueOf(MUNDANE_BITS));
+            addOverride(path, properties);
+        }
+    }
+
+    private void registerVanillaPotion(Identifier path, int damage, int mask, boolean splash) {
+        Properties properties = newProperties(path, ITEM_ID_POTION, splash);
+        properties.setProperty("damage", String.valueOf(damage));
+        properties.setProperty("damageMask", String.valueOf(mask));
+        addOverride(path, properties);
+    }
+
+    private void registerCustomPotion(Identifier path, RegistryEntry<StatusEffect> effect, boolean splash) {
+        Properties properties = newProperties(path, ITEM_ID_POTION, splash);
+        properties.setProperty("nbt.CustomPotionEffects.0.Id", String.valueOf(PotionIdUtils.getEffectId(effect)));
+        addOverride(path, properties);
+    }
+
+    private void registerEmptyBottle(Identifier path) {
+        Properties properties = newProperties(path, ITEM_ID_GLASS_BOTTLE, LAYER_EMPTY_BOTTLE);
+        addOverride(path, properties);
+    }
+
+    private void addOverride(Identifier path, Properties properties) {
+        Identifier propertiesName = TexturePackAPI.transformIdentifier(path, ".png", ".properties");
+        ItemOverride override = new ItemOverride(new PropertiesFile(logger, propertiesName, properties));
+        if (override.properties.valid()) {
+            overrides.add(override);
+        }
+    }
+}
---
+++ src/main/java/org/fabricmcpatcher/utils/id/PotionIdUtils.java
@@ -0,0 +1,158 @@
+package org.fabricmcpatcher.utils.id;
+
+import com.ibm.icu.impl.ICUResourceBundle;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
+import net.minecraft.registry.entry.RegistryEntry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PotionIdUtils {
+
+    public static final Map<String,PotionInfo> potionName2Info = new HashMap<>();
+    public static final Map<RegistryEntry<Potion>,PotionInfo> potionType2Info = new HashMap<>();
+    public static final Map<RegistryEntry<Potion>,Integer> potionType2Damage = new HashMap<>();
+    public static Map<RegistryEntry<StatusEffect>,Integer> effect2Id = new HashMap<>();
+
+    public static final String[] MUNDANE_NAMES = new String[]{
+            "potion.prefix.mundane",
+            "potion.prefix.uninteresting",
+            "potion.prefix.bland",
+            "potion.prefix.clear",
+            "potion.prefix.milky",
+            "potion.prefix.diffuse",
+            "potion.prefix.artless",
+            "potion.prefix.thin",
+            "potion.prefix.awkward",
+            "potion.prefix.flat",
+            "potion.prefix.bulky",
+            "potion.prefix.bungling",
+            "potion.prefix.buttered",
+            "potion.prefix.smooth",
+            "potion.prefix.suave",
+            "potion.prefix.debonair",
+            "potion.prefix.thick",
+            "potion.prefix.elegant",
+            "potion.prefix.fancy",
+            "potion.prefix.charming",
+            "potion.prefix.dashing",
+            "potion.prefix.refined",
+            "potion.prefix.cordial",
+            "potion.prefix.sparkling",
+            "potion.prefix.potent",
+            "potion.prefix.foul",
+            "potion.prefix.odorless",
+            "potion.prefix.rank",
+            "potion.prefix.harsh",
+            "potion.prefix.acrid",
+            "potion.prefix.gross",
+            "potion.prefix.stinky"};
+
+    static {
+        createPotion("moveSpeed", StatusEffects.SPEED,8194,Potions.SWIFTNESS,8258,Potions.LONG_SWIFTNESS,8226,Potions.STRONG_SWIFTNESS);
+        createPotion("moveSlowdown",StatusEffects.SLOWNESS, 8202,Potions.SLOWNESS,8266,Potions.LONG_SLOWNESS,0,Potions.STRONG_SLOWNESS);
+        createPotion("digSpeed",StatusEffects.HASTE);
+        createPotion("digSlowDown",StatusEffects.MINING_FATIGUE);
+        createPotion("damageBoost",StatusEffects.STRENGTH,8201,Potions.STRENGTH,8265,Potions.LONG_STRENGTH,8233,Potions.STRONG_STRENGTH);
+        createPotion("heal",StatusEffects.INSTANT_HEALTH,8197,Potions.HEALING, 8229,Potions.STRONG_HEALING);
+        createPotion("harm",StatusEffects.INSTANT_DAMAGE,8204,Potions.HARMING,8236,Potions.STRONG_HARMING);
+        createPotion("jump",StatusEffects.JUMP_BOOST,8203,Potions.LEAPING,8267,Potions.LONG_LEAPING,8235,Potions.STRONG_LEAPING);
+        createPotion("confusion",StatusEffects.NAUSEA);
+        createPotion("regeneration",StatusEffects.REGENERATION,8193,Potions.REGENERATION,8257,Potions.LONG_REGENERATION,8225,Potions.STRONG_REGENERATION);
+        createPotion("resistance",StatusEffects.RESISTANCE);
+        createPotion("fireResistance",StatusEffects.FIRE_RESISTANCE,8195,Potions.FIRE_RESISTANCE,8259,Potions.LONG_FIRE_RESISTANCE);
+        createPotion("waterBreathing",StatusEffects.WATER_BREATHING,8205,Potions.WATER_BREATHING,8269,Potions.LONG_WATER_BREATHING);
+        createPotion("invisibility",StatusEffects.INVISIBILITY,8206,Potions.INVISIBILITY,8270,Potions.LONG_INVISIBILITY);
+        createPotion("blindness",StatusEffects.BLINDNESS);
+        createPotion("nightVision",StatusEffects.NIGHT_VISION,8198,Potions.NIGHT_VISION,8262,Potions.LONG_NIGHT_VISION);
+        createPotion("hunger",StatusEffects.HUNGER);
+        createPotion("weakness",StatusEffects.WEAKNESS,8200,Potions.WEAKNESS,8264,Potions.LONG_WEAKNESS);
+        createPotion("poison",StatusEffects.POISON,8196,Potions.POISON,8260,Potions.LONG_POISON,8228,Potions.STRONG_POISON);
+        createPotion("wither",StatusEffects.WITHER);
+        createPotion("healthBoost",StatusEffects.HEALTH_BOOST);
+        createPotion("absorption",StatusEffects.ABSORPTION);
+        createPotion("saturation",StatusEffects.SATURATION);
+        createPotion("water",null,0,Potions.WATER,16,Potions.AWKWARD,32,Potions.THICK,64,Potions.MUNDANE);
+//TURTLE_MASTER
+//LUCK
+//SLOW_FALLING
+//WIND_CHARGED
+//WEAVING
+//OOZING
+//INFESTED
+    }
+
+    private static int nextEffectId = 0;
+
+    private static void createPotion(String name, RegistryEntry<StatusEffect> effect,Object... potionIds) {
+        PotionInfo info = new PotionInfo();
+
+        if(effect!=null)
+            effect2Id.put(effect,nextEffectId++);
+
+        info.effect = effect;
+        info.name = name;
+
+        if(potionIds.length!=0)
+            info.hasItemForm=true;
+
+        potionName2Info.put(name,info);
+        for (int i = 0; i < potionIds.length/2; i++) {
+            if(info.type2Damage==null)
+                info.type2Damage=new HashMap<>();
+
+            RegistryEntry<Potion> potion = (RegistryEntry<Potion>) potionIds[i*2+1];
+            int damage = (int) potionIds[i*2];
+
+            potionType2Damage.put(potion,damage);
+
+            info.type2Damage.put(potion,damage);
+            potionType2Info.put(potion,info);
+        }
+    }
+
+    public static int getPotionDamage(RegistryEntry<Potion> potion,boolean splash) {
+
+        Integer damage = potionType2Damage.get(potion);
+
+        if(damage==null)return -1;
+
+        //has drinkable bit
+        if((damage&(0b1111111111111+1))!=0) {
+
+            if(!splash)
+                return damage;
+
+            damage ^= 0b1111111111111+1;//remove it
+
+        }
+        else if(damage==0)
+            return 0;//splash water didnt exist in damage value form
+
+        if(splash)
+            damage |= 16384;
+        else
+            damage |= 8192;
+
+        return damage;
+    }
+
+    public static int getEffectId(RegistryEntry<StatusEffect> effect) {
+        return effect2Id.getOrDefault(effect,-1);
+    }
+
+    public static class PotionInfo {
+        public String name;
+        public Map<RegistryEntry<Potion>,Integer> type2Damage=null;
+        public RegistryEntry<StatusEffect> effect;
+        public boolean hasItemForm = false;
+    }
+
+    //potion helper replacement
+    public static String getMundaneName(int id) {
+        return MUNDANE_NAMES[id>>1];//bit 1 -> ???
+    }
+}
